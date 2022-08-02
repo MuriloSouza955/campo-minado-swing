0c21fa89547e153c08e5fba0ba89279b748208bb
Windows <souza.murilo@live.com>
2022-08-01 21:18:53 -0300

    Cap13_Aula11

--- src/br/com/MKCM/cm/visao/BotaoCampo.java
+++ src/br/com/MKCM/cm/visao/BotaoCampo.java
@@ -1,12 +1,74 @@
 package br.com.MKCM.cm.visao;
 
 import br.com.MKCM.cm.modelo.Campo;
+import br.com.MKCM.cm.modelo.CampoEvento;
+import br.com.MKCM.cm.modelo.CampoObservador;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class BotaoCampo extends JButton {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
+
+    private final Color BG_PADRAO = new Color(184, 184, 184);
+    private final Color BG_MARCAR = new Color(8, 179, 247);
+    private final Color BG_EXPLODIR = new Color(189, 66, 68);
+    private final Color TEXTO_VERDE = new Color(0, 150, 0);
+
+    private Campo campo;
 
     public BotaoCampo(Campo campo){
+        this.campo = campo;
+        setBackground(BG_PADRAO);
+        setBorder(BorderFactory.createBevelBorder(0));
+    }
+
+    @Override
+    public void eventoOcorreu(Campo campo, CampoEvento evento) {
+        switch (evento){
+            case ABRIR -> aplicarEstiloAbrir();
+            case MARCAR -> aplicarEstiloMarcar();
+            case EXPLODIR -> aplicarEstiloExplodir();
+            default -> aplicarEstiloPadrao();
+            
+        }
+    }
+
+    private void aplicarEstiloPadrao() {
+    }
+
+    private void aplicarEstiloExplodir() {
+    }
+
+    private void aplicarEstiloMarcar() {
+    }
+
+    private void aplicarEstiloAbrir() {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
 
     }
 }
